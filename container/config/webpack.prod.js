5d7815650c18808691eb945a800dd343c24949c9
haonn <hao.nguyennhat97@gmail.com>
2025-10-06 19:45:13 +0700

    update public path 2

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/container/lastest/'
+        publicPath: '/container/lastest'
     },
     plugins: [
         new ModuleFederationPlugin({

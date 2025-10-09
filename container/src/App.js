c64df5c174fc133a4fdb433f7363e5c8cca6e335
haonn <hao.nguyennhat97@gmail.com>
2025-10-09 16:54:07 +0700

    Add header in container module

--- container/src/App.js
+++ container/src/App.js
@@ -1,8 +1,16 @@
 'use strict'
 import React from 'react'
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import {BrowserRouter} from 'react-router-dom'
+
 export default () => {
     return (
-        <MarketingApp/>
+        <BrowserRouter>
+            <div>
+                <Header/>
+                <MarketingApp/>
+            </div>
+        </BrowserRouter>
     )
 }

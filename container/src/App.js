1ba349d1eee55bbca9914fa2051322d435343d67
haonn <hao.nguyennhat97@gmail.com>
2025-10-09 17:10:43 +0700

    Add prefix of css

--- container/src/App.js
+++ container/src/App.js
@@ -3,14 +3,20 @@ import React from 'react'
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import {BrowserRouter} from 'react-router-dom'
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
 
+const productionClassname = createGenerateClassName({
+    productionPrefix: 'container-'
+})
 export default () => {
     return (
-        <BrowserRouter>
-            <div>
-                <Header/>
-                <MarketingApp/>
-            </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={productionClassname}>
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <MarketingApp/>
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     )
 }
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,13 +1,15 @@
 'use strict'
 import React from 'react'
 import {Switch, BrowserRouter, Route} from 'react-router-dom';
-import {StylesProvider} from '@material-ui/core/styles'
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
-
+const  productionClassname = createGenerateClassName({
+    productionPrefix: 'marketing-'
+})
 export default () => {
     return (<div>
-        <StylesProvider>
+        <StylesProvider generateClassName={ productionClassname}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/" component={Landing}/>
